5fcd04a17c4e2b1352faf92d0cb9e9b3adedb01c
Johanna Ruiz <rbjoha@gmail.com>
2018-02-16 13:31:18 -0600

    Con IIS

--- servidor/Globals.cs
+++ servidor/Globals.cs
@@ -7,7 +7,7 @@ namespace servidor
 {
     public class Globals
     {  
-        private static string ip = @"Data Source= 192.168.2.116;Initial Catalog=mantenimiento;User Id=sa;Password=123";
+        private static string ip = @"Data Source= 172.24.20.41;Initial Catalog=mantenimiento;User Id=sa;Password=c0mun1d@d";
 
 
         public static string IP
